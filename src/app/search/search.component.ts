65a7fe9499b3726e951f46c99a681df2b5d86c32
Peter Erdodi <peter.erdodi@exputec.com>
2018-11-07 12:15:22 +0100

    Added skeleton for search

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  search(event: any, target: string, query: string) {
+    console.log(event);
+    console.log(target);
+    console.log(query);
+  }
+}
